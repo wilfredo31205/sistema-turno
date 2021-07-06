bc86e246db5d3bf065aa92b73790384391a3925e
wilfredo <wilfredofrias146@gmail.com>
2021-07-06 16:19:11 -0400

    primera

---
+++ models/sockets.js
@@ -0,0 +1,56 @@
+const TicketList = require('./ticket-list');
+
+
+class Sockets {
+
+    constructor( io ) {
+
+        this.io = io;
+
+        // Crear la instancia de nuestro ticketlist
+        this.ticketList = new TicketList();
+
+        this.socketEvents();
+        
+    }
+
+    socketEvents() {
+        // On connection
+        this.io.on( 'connection', ( socket ) => {
+
+           console.log('cliente conectado');
+
+           socket.on( 'solicitar-ticket' , (data, callback) => {
+                const nuevoTicket = this.ticketList.crearTicket();
+                callback( nuevoTicket );
+
+            
+           });
+
+           socket.on( 'siguiente-ticket-trabajar', ({ agente, escritorio }, callback) => {
+
+
+               // Emitir audio 
+
+              // const audio = new Audio('/Audio/ticket.mp3');
+              // audio.play();
+
+
+
+
+
+                const suTicket = this.ticketList.asignarTicket( agente, escritorio ); 
+                callback( suTicket );
+
+                this.io.emit( 'ticket-asignado', this.ticketList.ultimos13 );
+           })
+            
+        
+        });
+    }
+
+
+}
+
+
+module.exports = Sockets;
